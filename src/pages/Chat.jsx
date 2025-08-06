5106bee31ff79435ded42764cdc9af8f7a0378ba
GianeLopez.dev <gianylopez8@gmail.com>
2025-08-05 21:32:06 -0300

    Proyecto Whatsapp Marvel - detalles

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -3,47 +3,82 @@ import { useContext } from 'react';
 import { ChatContext } from '../context/ChatContext';
 import MessageForm from '../components/MessageForm';
 
+const users = [
+  {
+    id: '1',
+    name: 'Iron Man',
+    avatar: '/public/ironman.jpg'
+  },
+  {
+    id: '2',
+    name: 'Captain America',
+    avatar: '/public/cap.jpg'
+  },
+  {
+    id: '3',
+    name: 'Black Widow',
+    avatar: '/public/widow.jpg'
+  }
+];
+
+
 export default function Chat() {
   const { userId } = useParams();
   const { messages } = useContext(ChatContext);
 
+  const user = users.find(u => u.id === userId);
+
   return (
-    <div style={{ padding: '1rem', maxWidth: '600px', margin: '0 auto' }}>
-      <h2>Chateando con {userId}</h2>
-      
-      <div className="container">
+    <div className="container">
+      <h2 style={{ margin: 0 }}>
+    🦸 Chat con: <span style={{ color: '#e62429' }}>{user?.name || 'Desconocido'}</span>
+    </h2>
+
       <div style={{
         display: 'flex',
-        flexDirection: 'column',
-        gap: '0.5rem',
-        maxHeight: '70vh',
-        overflowY: 'auto',
+        alignItems: 'center',
+        gap: '1rem',
+        marginBottom: '1rem',
+        backgroundColor: '#1e1e2e',
         padding: '1rem',
-        background: '#1e1e2e',
-        borderRadius: '8px'
+        borderRadius: '10px',
+        justifyContent: 'center'
         }}>
+        <img
+        src={user?.avatar}
+        alt={user?.name}
+        style={{width: '50px',
+        height: '50px',
+        borderRadius: '50%',
+        objectFit: 'cover'
+    }}
+  />
         {(messages[userId] || []).map((msg, i) => (
-          <div key={i} className={`bubble ${msg.from}`}>
-          {msg.text}
-          </div>
-          ))}
-        </div>
-        </div>
-        <MessageForm userId={userId} />
-        <div style={{ textAlign: 'center', marginTop: '1.5rem' }}>
-        <Link
-        to="/"
-        style={{
-          padding: '0.5rem 1rem',
-          background: '#333',
-          color: '#fff',
-          borderRadius: '5px',
-          display: 'inline-block'
-          }}
+          <div
+            key={i}
+            className={`bubble ${msg.from}`}
           >
-          ← Volver al inicio
-          </Link>
+            {msg.text}
           </div>
-        </div>
-        );
+        ))}
+      </div>
+
+      <MessageForm userId={userId} />
+
+      <div style={{ textAlign: 'center', marginTop: '1.5rem' }}>
+        <Link
+          to="/"
+          style={{
+            padding: '0.5rem 1rem',
+            background: '#333',
+            color: '#fff',
+            borderRadius: '5px',
+            display: 'inline-block'
+          }}
+        >
+          ← Volver al inicio
+        </Link>
+      </div>
+    </div>
+  );
 }
